49a7e7e7ecbbd16a303bf8e686eaeafba9325b75
MichalIzb <michalizb@wp.pl>
2023-03-04 13:10:59 +0100

    added bootstrap and logic who win

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoardComponent implements OnInit {
   squares: any[] = [];
-  xIsNext: boolean = false;
+  xIsNext: boolean = true;
   winner: string = "";
 
   constructor() { }
@@ -15,16 +15,47 @@ export class BoardComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  newGame(){
+  newGame() {
     this.squares = Array(9).fill('null');
     this.winner = "";
     this.xIsNext = true;
   }
 
-  get player(){
-    return this.xIsNext ? 'X': 'O';
+  get player() {
+    return this.xIsNext ? 'X' : 'O';
   }
 
+  makeMove(id: number) {
+    if (!this.squares[id]) {
+      this.squares.splice(id, 1, this.player);
+      this.xIsNext = !this.xIsNext;
+    }
+    this.winner = this.calculateWinner();
+  }
 
+  calculateWinner()
+  {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        this.squares[a] &&
+        this.squares[a] === this.squares[b] &&
+        this.squares[a] === this.squares[c]
+      ) {
+        return this.squares[a];
+      }
+    }
+    return null;
+  }
 
 }
